8b88db832a03fbbe75b910503c58a675f12bc2e5
guillecg <guillemcg92@gmail.com>
2026-07-02 11:40:28 +0100

    Add build_profiles function

--- src/hmmvle/hmm.py
+++ src/hmmvle/hmm.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+import pyhmmer
 import pyfamsa
 
 
@@ -15,3 +16,31 @@ def build_msa(df: pd.DataFrame) -> pyfamsa.Alignment:
     msa = aligner.align(sequences)
 
     return msa
+
+
+def build_profiles(
+    df: pd.DataFrame,
+    name: str,
+    alphabet: pyhmmer.easel.Alphabet
+) -> pyhmmer.plan7.HMM:
+
+    msa = build_msa(df)
+
+    # Covert to TextMSA to digitize it
+    msa_text  = pyhmmer.easel.TextMSA(
+        name=name.encode(),
+        sequences=[
+            pyhmmer.easel.TextSequence(
+                name=seq.id,
+                sequence=seq.sequence.decode()
+            )
+            for seq in msa
+        ]
+    )
+
+    builder = pyhmmer.plan7.Builder(alphabet)
+    background = pyhmmer.plan7.Background(alphabet)
+
+    hmm, _, _ = builder.build_msa(msa_text.digitize(alphabet), background)
+
+    return hmm
