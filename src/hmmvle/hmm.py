a76f90bb2e78749b7010f8ed8371b8a26fd6addc
guillecg <guillemcg92@gmail.com>
2026-09-09 13:21:13 +0200

    Move define_hits function to hmm.py

--- src/hmmvle/hmm.py
+++ src/hmmvle/hmm.py
@@ -126,3 +126,18 @@ def get_seqs(filepath: str) -> pd.DataFrame:
             )
 
     return pd.concat(seqs)
+
+
+def define_hits(
+    row: pd.Series,
+    hmm_thr: float,
+    group: str
+) -> str:
+    # There are no false negatives because by definition the threshold includes
+    # all the considered true positives (threshold is minimum among them)
+    if row["score_full_seq"] >= hmm_thr and row["group"] == group:
+        return "True positive"
+    elif row["score_full_seq"] >= hmm_thr and row["group"] != group:
+        return "False positive"
+    else:
+        return "True negative"
