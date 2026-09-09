6db81682ea6a578f19fedc000637ef1cd41e755a
guillecg <guillemcg92@gmail.com>
2026-09-09 17:22:06 +0200

    Move define_hits function to hyddb.py

--- src/hmmvle/preprocessing/hyddb.py
+++ src/hmmvle/preprocessing/hyddb.py
@@ -55,3 +55,18 @@ def process_hyddb(filepath: str) -> pd.DataFrame:
         )
 
     return pd.concat(metadata_df)
+
+
+def define_hits(
+    row: pd.Series,
+    hmm_thr: float,
+    group: str
+) -> str:
+    # There are no false negatives because by definition the threshold includes
+    # all the considered true positives (threshold is minimum among them)
+    if row["score_full_seq"] >= hmm_thr and row["group"] == group:
+        return "True positive"
+    elif row["score_full_seq"] >= hmm_thr and row["group"] != group:
+        return "False positive"
+    else:
+        return "True negative"
