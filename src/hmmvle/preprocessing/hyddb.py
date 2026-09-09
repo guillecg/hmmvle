5654afff1f3f030fe47b144b6405b4ebab4a1f1a
guillecg <guillemcg92@gmail.com>
2026-09-09 18:28:47 +0200

    Rename group argument to hmm_group

--- src/hmmvle/preprocessing/hyddb.py
+++ src/hmmvle/preprocessing/hyddb.py
@@ -60,13 +60,13 @@ def process_hyddb(filepath: str) -> pd.DataFrame:
 def define_hits(
     row: pd.Series,
     hmm_thr: float,
-    group: str
+    hmm_group: str
 ) -> str:
     # There are no false negatives because by definition the threshold includes
     # all the considered true positives (threshold is minimum among them)
-    if row["score_full_seq"] >= hmm_thr and row["group"] == group:
+    if row["score_full_seq"] >= hmm_thr and row["group"] == hmm_group:
         return "True positive"
-    elif row["score_full_seq"] >= hmm_thr and row["group"] != group:
+    elif row["score_full_seq"] >= hmm_thr and row["group"] != hmm_group:
         return "False positive"
     else:
         return "True negative"
